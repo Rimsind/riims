f6d2a1b42d336f17cef337a106ba5ca3dd8d5ab3
Rims <rimstech21@gmail.com>
2021-12-12 18:12:27 +0530

    012

--- components/common/Loading.js
+++ components/common/Loading.js
@@ -15,7 +15,7 @@ const Loading = () => {
             className="col-md-4"
             style={{ marginTop: "200px", marginLeft: "220px" }}
           >
-            <div className=""="loader"></div>
+            <div className="loader"></div>
           </div>
           <div className="col-md-4"></div>
         </div>
